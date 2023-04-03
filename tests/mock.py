cd76fd1c711988200bd87932c55bad3f99fc419b
Martin Storgaard Dieu <Mastdieu@protonmail.com>
2023-04-03 22:33:15 +0200

    Add MockStreamWriter and create_open_connection_mock

--- tests/mock.py
+++ tests/mock.py
@@ -10,3 +10,27 @@ class MockStreamReader(asyncio.StreamReader):
         part = self._stream[self._index : self._index + n]
         self._index += n
         return part
+
+
+class MockStreamWriter(asyncio.StreamWriter):
+    def __init__(self):
+        self.received_bytes = b""
+
+    def write(self, data: bytes) -> None:
+        self.received_bytes += data
+
+    def close(self) -> None:
+        pass
+
+    async def wait_closed(self) -> None:
+        pass
+
+    async def drain(self) -> None:
+        pass
+
+
+def create_open_connection_mock(reader, writer):
+    async def _open_connection():
+        return reader, writer
+
+    return _open_connection
