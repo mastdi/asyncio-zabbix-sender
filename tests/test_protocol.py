136d7ab6f31174b0bd47b2be76c39bcf73912e77
Martin Storgaard Dieu <Mastdieu@protonmail.com>
2023-04-03 22:14:01 +0200

    Add Zabbix protocol

---
+++ tests/test_protocol.py
@@ -0,0 +1,90 @@
+import asyncio
+import struct
+import zlib
+
+import pytest
+
+from zabbix_sender import create_packet
+from zabbix_sender._protocol import PacketParts, parse_packet_parts, read_response
+
+
+def test_create_packet_without_compression():
+    request = b'{"hello":"world"}'
+
+    packet = create_packet(request, False)
+
+    flags, data_length, reserved = struct.unpack("<BII", packet[4 : -len(request)])
+    assert packet.startswith(b"ZBXD")
+    assert flags == 0x01
+    assert data_length == len(request)
+    assert reserved == 0
+    assert packet.endswith(request)
+
+
+def test_create_packet_with_compression():
+    request = b'{"hello":"world"}'
+    expected_data = zlib.compress(request)
+
+    packet = create_packet(request, True)
+
+    flags, data_length, reserved = struct.unpack(
+        "<BII", packet[4 : -len(expected_data)]
+    )
+    assert packet.startswith(b"ZBXD")
+    assert flags == 0x01 | 0x02
+    assert data_length == len(expected_data)
+    assert reserved == len(request)
+    assert packet.endswith(expected_data)
+
+
+class MockStreamReader(asyncio.StreamReader):
+    def __init__(self, stream: bytes) -> None:
+        self._stream = stream
+        self._index = 0
+
+    async def readexactly(self, n: int) -> bytes:
+        part = self._stream[self._index : self._index + n]
+        self._index += n
+        return part
+
+
+@pytest.mark.asyncio
+async def test_read_response():
+    request = b'{"hello":"world"}'
+    packet = create_packet(request, False)
+    reader = MockStreamReader(packet)
+
+    response = await read_response(reader)
+
+    assert response.data == request
+    assert response.data_length == len(request)
+    assert response.flags == 0x01
+    assert response.protocol == b"ZBXD"
+    assert response.reserved == 0
+
+
+def test_parse_packet_parts():
+    request = b'{"hello":"world"}'
+    packet_parts = PacketParts(
+        data=request, data_length=len(request), flags=0x01, protocol=b"ZBXD", reserved=0
+    )
+
+    parsed_request = parse_packet_parts(packet_parts)
+
+    assert parsed_request == {"hello": "world"}
+
+
+def test_parse_packet_parts_with_compression():
+    request = b'{"hello":"world"}'
+    compressed_request = zlib.compress(request)
+    packet_parts = PacketParts(
+        data=compressed_request,
+        data_length=len(compressed_request),
+        flags=0x01 | 0x02,
+        protocol=b"ZBXD",
+        reserved=len(request),
+    )
+
+    parsed_request = parse_packet_parts(packet_parts)
+
+    assert parsed_request == {"hello": "world"}
---
+++ zabbix_sender/_protocol.py
@@ -0,0 +1,60 @@
+import asyncio
+import json
+import struct
+import typing
+import zlib
+
+
+class PacketParts(typing.NamedTuple):
+    protocol: bytes
+    flags: int
+    data_length: int
+    reserved: int
+    data: bytes
+
+
+def create_packet(request: bytes, use_compression: bool) -> bytes:
+    """Create a Zabbix packet from a request.
+
+    :param request: The encoded JSON body
+    :param use_compression: If True, the request is compressed
+    :return: The full packet ready to be sent to Zabbix
+    """
+    protocol = b"ZBXD"
+    flags = 0x01
+    data = request
+    reserved = 0
+    if use_compression:
+        flags |= 0x02
+        data = zlib.compress(request)
+        reserved = len(request)
+
+    return protocol + struct.pack("<BII", flags, len(data), reserved) + data
+
+
+async def read_response(reader: asyncio.StreamReader) -> PacketParts:
+    """Read response from the open stream.
+
+    :param reader: The stream reader connected to a Zabbix Server/Proxy.
+    :return: The packet.
+    """
+    header = await reader.readexactly(13)
+    assert header.startswith(b"ZBXD"), header
+    flags, data_length, reserved = struct.unpack("<BII", header[4:])
+
+    data = await reader.readexactly(data_length)
+    return PacketParts(b"ZBXD", flags, data_length, reserved, data)
+
+
+def parse_packet_parts(packet_parts: PacketParts) -> typing.Dict[str, str]:
+    """Parse the packet parts and the JSON payload to a dict.
+
+    :param packet_parts: The packet parts
+    :return: The parsed JSON payload
+    """
+    data = packet_parts.data
+    if packet_parts.flags & 0x02 > 0:
+        data = zlib.decompress(packet_parts.data)
+        assert len(data) == packet_parts.reserved
+
+    return json.loads(data)
