cbe131e212ab1d2473218c0ca8be01f1b69202fc
Martin Storgaard Dieu <Mastdieu@protonmail.com>
2024-02-29 21:00:46 +0100

    Update copyright

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2023. Martin Storgaard Dieu <martin@storgaarddieu.com>
+#  Copyright (c) 2024. Martin Storgaard Dieu <martin@storgaarddieu.com>
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
